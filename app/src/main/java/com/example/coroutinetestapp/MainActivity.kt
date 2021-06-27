49d0c2f5fe4d90ac5826dcdf1f8da9b66a176380
kakao <gywn6209@gmail.com>
2021-06-27 15:52:37 +0900

    add viewbinding in MainActivity.kt

--- app/src/main/java/com/example/coroutinetestapp/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetestapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.coroutinetestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coroutinetestapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    val netDispatcher = newSingleThreadContext(name = "ServiceCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(binding.root)
     }
 }
